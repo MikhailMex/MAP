760e7dfb4e7b69ada3ccf95ebdf0af1b75fc388c
Yandex_Lyceum_D2 <irongaga@gmail.com>
2024-02-12 19:53:00 +0300

    2 задача

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ class Example(QWidget):
             file.write(response.content)
 
     def initUI(self):
+        self.x = 600
+        self.y = 450
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
@@ -38,12 +40,29 @@ class Example(QWidget):
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
-        self.image.resize(600, 450)
+        self.image.resize(self.x, self.y)
         self.image.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.x += 10
+            self.y += 10
+            self.update_image()
+        elif event.key() == Qt.Key_PageDown:
+            self.x -= 10
+            self.y -= 10
+            self.update_image()
+
+    def update_image(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(self.x, self.y)
+        self.image.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
